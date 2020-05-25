e0e627becaa072f21a27a0bbb076c73315ed7c7f
Belgin Android <61349423+Belgin-Android@users.noreply.github.com>
2020-05-25 13:57:30 +0530

    Update Text_To_Speech.py

--- Text_To_Speech.py
+++ Text_To_Speech.py
@@ -3,7 +3,10 @@ from gtts import gTTS
 import os
 f=open("test.txt") #The File Which It Will Read
 text=f.read()
+# Change The Language If Needed And Change slow=True if you Want the speed to be increased
 out=gTTS(text=text,lang='en',slow=False)
+# Saves the File In The Programs Destination
 out.save("spc.mp3")
+# For Playing The File ( Its Plays the song via the default MP3 Player )
 os.system("start spc.mp3")
  
